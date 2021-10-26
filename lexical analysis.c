f0d86ebcd2c973422060188d024547bfa954286c
vishnuoum <57527558+vishnuoum@users.noreply.github.com>
2021-10-26 15:49:22 +0530

    Update lexical analysis.c

--- lexical analysis.c
+++ lexical analysis.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <regex.h>
+#include <ctype.h>
+
 #define TRUE 1
 #define FALSE 0
+
+regex_t regex;
+
 void parse(char[]);
 void token(char[], int, int);
 int keyword(char[]);
@@ -12,6 +18,7 @@ int literal(char[]);
 
 void main(){
     char statement[100];
+    regcomp(&regex,"[:word:]",0);
     printf("Lexeme\tToken\n\n\n");
     FILE *fptr;
     fptr = fopen("files/input.c", "r");
@@ -60,13 +67,16 @@ void token(char statement[], int start, int end){
     else if (literal(token) == TRUE){
         printf("%s\tLiteral\n", token);
     }
-    else{
+    else if((token[0]=='_'||isalpha(token[0]))&&!(regexec(&regex, token, 0, NULL, 0))){
         printf("%s\tIdentifier\n", token);
+    }
+    else{
+        printf("%s\tNot recognized\n",token);
     }
 }
 
 int keyword(char tokenWord[]){
-    char keywords[50][50] = {"auto", "double", "int", "struct", "break", "else", "long", "switch", "case", "char", "return", "union", "continue", "for", "void", "do", "if", "static", "while", "default", "const", "float","main"};
+    char keywords[][50] = {"main","auto", "double", "int", "struct", "break", "else", "long", "switch", "case", "char", "return", "union", "continue", "for", "void", "do", "if", "static", "while", "default", "const", "float"};
     for (int i = 0; i < 23; i++){
         if (!strcmp(keywords[i], tokenWord)){
             return TRUE;
@@ -84,7 +94,7 @@ int delimiter(char tokenWord[]){
 
 int paranthesis(char tokenWord[])
 {
-    char paranth[50][5]={"(",")","{","}","[","]"};
+    char paranth[][5]={"(",")","{","}","[","]"};
     for (int i = 0; i < 6; i++){
         if (!strcmp(paranth[i], tokenWord)){
             return TRUE;
@@ -94,7 +104,7 @@ int paranthesis(char tokenWord[])
 }
 
 int operator(char tokenWord[]){
-    char operators[50][10] = {"+", "-", "*", "/", "++", "--", "=", "==", "<", ">", "<=", ">=", "!=", "!", "&", "|", "&&", "||"};
+    char operators[][10] = {"+", "-", "*", "/", "++", "--", "=", "==", "<", ">", "<=", ">=", "!=", "!", "&", "|", "&&", "||"};
     for (int i = 0; i < 18; i++){
         if (!strcmp(operators[i], tokenWord)){
             return TRUE;
@@ -105,7 +115,7 @@ int operator(char tokenWord[]){
 
 int literal(char tokenWord[]){
     char ch = tokenWord[0];
-    char literals[50] = {'\'', '\"', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    char literals[] = {'\'', '\"', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
     for (int i = 0; i < 12; i++){
         if (ch == literals[i])
         {
